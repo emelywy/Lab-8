cb56b4d4da391993b6de39fa57d6cbaabfdfb38e
diana <dianaandroshchuk1@gmail.com>
2026-05-11 10:25:24 +0300

    Update TestBillRepository.java

--- src/test/java/org/example/colproj/labs/lab8/TestBillRepository.java
+++ src/test/java/org/example/colproj/labs/lab8/TestBillRepository.java
@@ -13,18 +13,18 @@ public class TestBillRepository {
     BillRepository billRepository = new BillRepository();
 
     @Test
-    public void Test1(){
+    public void test1(){
         assertEquals(6, 3 + 3);
     }
 
     @Test
-    public void TestGetBillById(){ // шукаємо рахунок за ID 1
+    public void testGetBillById(){ // шукаємо рахунок за ID 1
         Bill bill = billRepository.getById(1L);
         assertNotNull(bill);
     }
 
     @Test
-    public void TestGetAllBills(){
+    public void testGetAllBills(){
         List<Bill> list = billRepository.getAll();
         assertTrue(list.size() > 0); // переконуємось, що база даних не порожня
     }
